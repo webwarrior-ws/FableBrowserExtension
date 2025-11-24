564e451e350ff1c759f9cf90dfa6f33b7a9e987e
webwarrior-ws <reg@webwarrior.ws>
2025-11-24 11:54:14 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+﻿import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+    clearScreen: false,
+    server: {
+        watch: {
+            ignored: [
+                "**/*.fs" // Don't watch F# files
+            ]
+        }
+    }
+})
